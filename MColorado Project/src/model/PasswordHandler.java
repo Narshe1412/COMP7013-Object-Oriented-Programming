276a2bbd409c335d944462068eb769f77f9c4087
Manuel Colorado <manuel1412@gmail.com>
2018-03-25 19:13:14 +0100

    PasswordHandler, PatientList classes documentation
    Added comments @params @return
    Generated Javadoc

--- MColorado Project/src/model/PasswordHandler.java
+++ MColorado Project/src/model/PasswordHandler.java
@@ -6,34 +6,66 @@ import exception.PassException;
 
 import javax.crypto.Cipher;
 
+/**
+ * 
+ * @author Abhisek Jana
+ * @see <a href=
+ *      "http://www.adeveloperdiary.com/java/how-to-easily-encrypt-and-decrypt-text-in-java/">How
+ *      to easily encrypt and decrypt text in Java</a>
+ *
+ */
 public class PasswordHandler {
-	public static String encrypt(String strClearText,String strKey) throws Exception{
-		String strData="";
-		
+	/**
+	 * Encrypts a string of text using the javax crypto library, using Blowfish
+	 * algorithm
+	 * 
+	 * @param strClearText
+	 *            String of text that needs encryption
+	 * @param strKey
+	 *            String of text that will be used by the hashing algorithm to
+	 *            encrypt
+	 * @return a hashed encrypted string of text using the algorithm
+	 * @throws Exception
+	 */
+	public static String encrypt(String strClearText, String strKey) throws Exception {
+		String strData = "";
+
 		try {
-			SecretKeySpec skeyspec=new SecretKeySpec(strKey.getBytes(),"Blowfish");
-			Cipher cipher=Cipher.getInstance("Blowfish");
+			SecretKeySpec skeyspec = new SecretKeySpec(strKey.getBytes(), "Blowfish");
+			Cipher cipher = Cipher.getInstance("Blowfish");
 			cipher.init(Cipher.ENCRYPT_MODE, skeyspec);
-			byte[] encrypted=cipher.doFinal(strClearText.getBytes());
-			strData=new String(encrypted);
-			
+			byte[] encrypted = cipher.doFinal(strClearText.getBytes());
+			strData = new String(encrypted);
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new PassException("Fatal: Encryption problem during password handling.");
 		}
 		return strData;
 	}
-	
-	public static String decrypt(String strEncrypted,String strKey) throws Exception{
-		String strData="";
-		
+
+	/**
+	 * Decrypts a string of text using the javax crypto library, using Blowfish
+	 * algorithm
+	 * 
+	 * @param strClearText
+	 *            String of text that will be decrypted
+	 * @param strKey
+	 *            String of text that will be used by the hashing algorithm to
+	 *            decrypt
+	 * @return a decrypted string of text using the same parameters used to encrypt
+	 * @throws Exception
+	 */
+	public static String decrypt(String strEncrypted, String strKey) throws Exception {
+		String strData = "";
+
 		try {
-			SecretKeySpec skeyspec=new SecretKeySpec(strKey.getBytes(),"Blowfish");
-			Cipher cipher=Cipher.getInstance("Blowfish");
+			SecretKeySpec skeyspec = new SecretKeySpec(strKey.getBytes(), "Blowfish");
+			Cipher cipher = Cipher.getInstance("Blowfish");
 			cipher.init(Cipher.DECRYPT_MODE, skeyspec);
-			byte[] decrypted=cipher.doFinal(strEncrypted.getBytes());
-			strData=new String(decrypted);
-			
+			byte[] decrypted = cipher.doFinal(strEncrypted.getBytes());
+			strData = new String(decrypted);
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new PassException("There was a problem checking your password.");
--- MColorado Project/src/model/PatientList.java
+++ MColorado Project/src/model/PatientList.java
@@ -6,75 +6,125 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Handles the serializable list of customers in the system
+ * 
  * @author Manuel Colorado
  *
  */
-public class PatientList extends ArrayList<Patient> implements Serializable{
+public class PatientList extends ArrayList<Patient> implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	/**
 	 * Constructor
 	 */
 	public PatientList() {
 		super();
 	}
-	
+
 	/**
 	 * Locates the Patient object based on their name
-	 * @param name String that represents the name of the patient
-	 * @return the Patient object with all relevant details of the patient or null if it cannot finds it
+	 * 
+	 * @param name
+	 *            String that represents the name of the patient
+	 * @return the Patient object with all relevant details of the patient or null
+	 *         if it cannot finds it
 	 */
 	public Patient find(String name) {
-		for (Patient patient: this) {
+		for (Patient patient : this) {
 			if (patient.getName().equalsIgnoreCase(name)) {
 				return patient;
 			}
 		}
 		return null;
 	}
-	
-	public Patient add(String name, String address, String phone) {
-		Patient p = new Patient(name, address,phone); 
+
+	/**
+	 * Adds a new Patient object to the list of patients in the system
+	 * 
+	 * @param name
+	 *            String that represents the name of the patient
+	 * @param address
+	 *            String that represents the address of the patient
+	 * @param phone
+	 *            String that represents the phone of the patient
+	 * @return the reference to the patient already in the list
+	 */
+	public Patient add(final String name, final String address, final String phone) {
+		Patient p = new Patient(name, address, phone);
 		super.add(p);
 		p.setPatientNo(indexOf(p));
 		return p;
 	}
-	
-	public void update(Patient p, String name, String address, String phone) {
+
+	/**
+	 * Update a patient object in the system by providing the object reference
+	 * 
+	 * @param p
+	 *            Patient object reference to the patient in the list
+	 * @param name
+	 *            String that represents the name of the patient
+	 * @param address
+	 *            String that represents the address of the patient
+	 * @param phone
+	 *            String that represents the phone of the patient
+	 */
+	public void update(final Patient p, final String name, final String address, final String phone) {
 		p.setName(name);
 		p.setAddress(address);
 		p.setPhone(phone);
 	}
-	
-	public void updateById(int id, String name, String address, String phone) {
+
+	/**
+	 * Update a patient object in the system by providing the position in the list
+	 * 
+	 * @param id
+	 *            Id of the patient object in the list
+	 * @param p
+	 *            Patient object reference to the patient in the list
+	 * @param name
+	 *            String that represents the name of the patient
+	 * @param address
+	 *            String that represents the address of the patient
+	 * @param phone
+	 *            String that represents the phone of the patient
+	 */
+	public void updateById(final int id, final String name, final String address, final String phone) {
 		Patient toUpdate = get(id);
 		toUpdate.setName(name);
 		toUpdate.setAddress(address);
 		toUpdate.setPhone(phone);
 	}
-	
+
+	/**
+	 * Deletes a Patient object from the list provided its id
+	 * 
+	 * @param id
+	 *            numeric identifier to delete
+	 */
+	public void deleteById(final int id) {
+		super.remove(id);
+	}
+
+	/**
+	 * Delete a Patient object from the list using its reference
+	 * 
+	 * @param p
+	 *            reference to the Patient object in the list
+	 */
+	public void delete(final Patient p) {
+		super.remove(p);
+	}
+
+	/**
+	 * Override of the toString() method Obtains the list of patients in the format
+	 * "ID: Name Address: ... Phone: ... \n"
+	 */
+	@Override
 	public String toString() {
 		String result = "";
-		for (Patient patient: this) {
+		for (Patient patient : this) {
 			result += indexOf(patient) + ": " + patient.toString() + "\n";
 		}
 		return result;
 	}
 }
-
-//public class PatientList {
-/*	
-	private AtomicInteger id = new AtomicInteger();
-	
-	public PatientList() {
-		this(0);
-	}
-	
-	public PatientList(int id) {
-		this.id = (int)id;
-	}
-
-	public int getNextId() {
-		return id.getAndIncrement();
-	}*/
